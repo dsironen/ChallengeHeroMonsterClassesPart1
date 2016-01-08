83760aafeb6353e1a6302fc33a44d57e78730766
Daniel Sironen <daniel.sironen@gmail.com>
2016-01-08 16:08:42 -0500

    Starting Part1

---
+++ ChallengeHeroMonsterClassesPart1/Character.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChallengeHeroMonsterClassesPart1
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int DamageMaximum { get; set; }
+        public int AttackBonus { get; set; }
+        
+
+        public int Attack()
+        {
+            Random attack = new Random();
+            int damage = attack.Next(1, 10);
+            Health -= damage;
+            return Health;
+        }
+
+        public int Defend(int damage)
+        {
+            DamageMaximum += damage;
+            return DamageMaximum;
+        }
+    }
+}
---
+++ ChallengeHeroMonsterClassesPart1/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeHeroMonsterClassesPart1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Character hero = new Character();
+            hero.Name = "Daniel";
+            hero.Health = 100;
+            hero.DamageMaximum = 10;
+            hero.AttackBonus = 10;
+
+            hero.Attack();
+            resultLabel.Text = 
+
+            Character monster = new Character();
+            monster.Name = "Creep";
+            monster.Health = 100;
+            monster.DamageMaximum = 10;
+            monster.AttackBonus = 10;
+
+            monster.Attack();
+        }
+    }
+}
+    
